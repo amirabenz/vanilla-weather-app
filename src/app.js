387407ada0dd66d1cbc805e54195e1d8998a1f3f
amirabenz <amira.benzid09@gmail.com>
2022-02-27 20:18:12 +0100

    final update

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ function showForecast(response) {
 
 function getForecastInfos(coordinates) {
   let apiKey = "7a3a06bc53009599c7a0058ddd4c4727";
-  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
   axios.get(url).then(showForecast);
 }
 function change(response) {
@@ -87,7 +87,7 @@ function change(response) {
   document
     .querySelector(".img-icon")
     .setAttribute("alt", response.data.weather[0].description);
-  getForecastInfos(response.data.coord);
+  getForecastInfos(response.data.coord, "metric");
 }
 
 function showCurrentInfos(position) {
@@ -105,13 +105,38 @@ function searchCurrentWeather(event) {
 
 let buttonCurrent = document.querySelector("#here-now");
 buttonCurrent.addEventListener("click", searchCurrentWeather);
-
+function changeF(response) {
+  console.log("fahrenheit:", response.data);
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(
+    "#here"
+  ).innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  document.querySelector("#searched-city").value = response.data.name;
+  document.querySelector(".wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector(".humidity").innerHTML = response.data.main.humidity;
+  document.querySelector(".weather-description").innerHTML =
+    response.data.weather[0].main;
+  document
+    .querySelector(".img-icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector(".img-icon")
+    .setAttribute("alt", response.data.weather[0].description);
+  getForecastInfos(response.data.coord, "imperial");
+}
 function toFahrenheit(event) {
   event.preventDefault();
   let apiKey = "7a3a06bc53009599c7a0058ddd4c4727";
   let city = document.querySelector("#searched-city").value;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(url).then(change);
+  axios.get(url).then(changeF);
   document.querySelector("#celsius").innerHTML = "°C ";
   document.querySelector("#fahrenheit").innerHTML = "| <strong> °F </strong>";
   document.querySelector(".wind-unit").innerHTML = " mph";
